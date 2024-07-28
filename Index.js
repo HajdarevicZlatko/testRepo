071d79da64913991721f963713694c1c0ce1b618
zlatkohajdarevic laptop <zlatko.hajdarevic@assoftteam.com>
2024-07-28 13:28:05 +0200

    update

--- Index.js
+++ Index.js
@@ -4,19 +4,17 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-
-// Ruta za verifikaciju emaila
 app.get('/ServisDva', async (req, res) => {
     try {
       res.status(200).send({ message: 'servisDva radi' });
     } catch (err) {
       console.error(err);
-      res.status(400).send({ message: 'Invalid token' });
+      res.status(400).send({ message: 'servisDva radi ne radi' });
     }
   });
 
 
 
 app.listen(80, () => {
-  console.log(`Server running on port 80`);
+  console.log(`Server running on port 80 ServisDva`);
 });
